01c8881142bc8ff958913f651502e95525e1210d
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-08-17 19:50:00 -0500

    Tweaked config.
    Changed nozzle offset.
    Enabled Probe Offset Wizard

--- firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -1061,7 +1061,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET {35, 25, -1.7}
+#define NOZZLE_TO_PROBE_OFFSET {35, 25, -2.21}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
